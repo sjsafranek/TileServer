50ba70408fb1f1fde5cf84ddf2b567bcbe28bf95
City of Albuquerque <pcrickard@cabq.gov>
2016-03-21 13:30:15 -0600

    Update Main.go
    HTTPS option

--- Main.go
+++ Main.go
@@ -10,5 +10,6 @@ func main() {
 	router := NewRouter()
 
 	log.Fatal(http.ListenAndServe("0.0.0.0:8080", router))
+	//ListenAndServeTLS("0.0.0.0:8080", "myCertFile.pem", "myKeyFile.pem", router) error  FOR HTTPS
 
 }
