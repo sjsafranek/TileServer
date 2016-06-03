c0ac1f00a7c3e6805769280af8273094f529d6a5
Stefan Safranek <stefan@stefan>
2016-06-03 15:58:22 -0700

    check for database file

---
+++ server.js
@@ -0,0 +1,29 @@
+// Tile server using the node web framework Express (http://expressjs.com).
+// Requrements:
+// express
+// tilelive
+// tilelive-mapnik
+var express = require('express'); //.createServer();
+var app = express();
+// Middleware
+var tilelive = require('tilelive');
+require('tilelive-mapnik').registerProtocols(tilelive);
+var filename = __dirname + '/' + 'stylesheet.xml';
+tilelive.load('mapnik://' + filename, function(err, source) {
+    if (err) throw err;
+    app.get('/:z/:x/:y.*', function(req, res) {
+        source.getTile(req.param('z'), req.param('x'), req.param('y'), function(err, tile, headers) {
+            // `err` is an error object when generation failed, otherwise null.
+            // `tile` contains the compressed image file as a Buffer
+            // `headers` is a hash with HTTP headers for the image.
+            if (!err) {
+                res.send(tile);
+            } else {
+                res.send('Tile rendering error: ' + err + '\n');
+            }
+        });
+    });    
+});
+
+console.log('Listening on port: ' + 8888);
+app.listen(8888);
