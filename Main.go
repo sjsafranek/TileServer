b447f10a5aa3b2abbf3d263d8f5e11f813fd3e07
Stefan Safranek <stefan@stefan>
2016-06-02 17:14:58 -0700

    change server port

--- Main.go
+++ Main.go
@@ -3,13 +3,32 @@ package main
 import (
 	"log"
 	"net/http"
+	"flag"
 )
+
+var (
+	port        string
+)
+
+func init() {
+	// flag.Usage = func() {
+	// 	fmt.Println("Usage: mbtile_builder [method] [option]\n")
+	// 	fmt.Printf("Methods:\n")
+	// 	fmt.Printf("  install\n\tInstall requirements\n")
+	// 	fmt.Printf("\n")
+	// 	fmt.Printf("Defaults:\n")
+	// 	flag.PrintDefaults()
+	// }
+	flag.StringVar(&port, "port", "8888", "server [port")
+	flag.Parse()
+}
+
 
 func main() {
 
 	router := NewRouter()
 
-	log.Fatal(http.ListenAndServe("0.0.0.0:8888", router))
+	log.Fatal(http.ListenAndServe("0.0.0.0:" + port, router))
 	//ListenAndServeTLS("0.0.0.0:8080", "myCertFile.pem", "myKeyFile.pem", router) error  FOR HTTPS
 
 }
