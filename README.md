84880d23f7e1d880b836a1844ccd4c5515d119e5
Stefan Safranek <stefan@stefan>
2016-06-03 15:27:28 -0700

    readme updates

--- README.md
+++ README.md
@@ -9,3 +9,23 @@ The structure of the server was taken from [Making a RESTful JSON API in Go](htt
 
 The tileserver was built as a copy of the PHP version by [Bryan McBride](https://github.com/bmcbride/PHP-MBTiles-Server)
 
+
+
+## Building MB Tiles with Mapbox Tilelive
+
+### Resources
+ - https://www.npmjs.com/package/tl
+ - https://github.com/mapbox/tilelive
+
+### Install
+ - ``npm install -g tl mbtiles tilelive-http``
+
+### Usage
+ - ``tl copy -z [zoom_min] -Z [zoom_max] -b "[WGS84 bounding box]" "[http tileserver address]" mbtiles://.//[DATABASENAME]/mbtiles``
+ - Example: ``tl copy -z 0 -Z 15 -b "19.481506 49.050920 20.407791 49.319751" "http://a.tile.openstreetmap.org/{z}/{x}/{y}.png" mbtiles://./tatra.mbtiles``
+
+### Defaults
+ - ``-b BBOX, --bounds BBOX        WGS84 bounding box  [-180,-85.0511,180,85.0511]``
+ - ``-z ZOOM, --min-zoom ZOOM      Min zoom (inclusive)  [0]``
+ - ``-Z ZOOM, --max-zoom ZOOM      Max zoom (inclusive)  [22]``
+
--- install.sh
+++ install.sh
@@ -2,35 +2,35 @@
 
 export GOPATH="`pwd`"
 
-key="$1"
+echo "creating workspace..."
 
-case $key in
-    -i|--install)
-        echo "checking requirements..."
-        
-        if [ ! -d "`pwd`/bin" ]; then
-            mkdir bin
-        fi
+# Create required directories
+echo "creating directories..."
+if [ ! -d "`pwd`/bin" ]; then
+    mkdir bin
+fi
 
-        if [ ! -d "`pwd`/src" ]; then
-            mkdir src
-            mkdir src/tileserver
-            cp tileserver/* src/tileserver/
-        fi
+if [ ! -d "`pwd`/src" ]; then
+    mkdir src
+    mkdir src/tileserver
+fi
 
-        if [ ! -d "`pwd`/src/github.com/gorilla/mux" ]; then
-            echo "installing mux..."
-            go get github.com/gorilla/mux
-        fi
+# Copy source files
+echo "copying source files..."
+cp -R tileserver/* src/tileserver/
 
-        if [ ! -d "`pwd`/src/github.com/mattn/go-sqlite3" ]; then
-            echo "installing bolt..."
-            go get github.com/mattn/go-sqlite3
-        fi
 
-        echo "done!"
-    ;;
-    *)
-        echo "unknown option"
-    ;;
-esac
+# Download required libraries
+echo "checking requirements..."
+
+if [ ! -d "`pwd`/src/github.com/gorilla/mux" ]; then
+    echo "downloading gorilla mux..."
+    go get github.com/gorilla/mux
+fi
+
+if [ ! -d "`pwd`/src/github.com/mattn/go-sqlite3" ]; then
+    echo "downloading go-sqlite3..."
+    go get github.com/mattn/go-sqlite3
+fi
+
+echo "done!"
